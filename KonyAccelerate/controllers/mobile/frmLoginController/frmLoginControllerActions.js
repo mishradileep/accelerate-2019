de6e7254ffed28a99ed1fc6107ef8a3ae4e7f39d
Vijay Samanthapudi <vijay.samanthapudi@kony.com>
2019-07-30 12:25:54 +0530

    [MARVP-5] Integrate Linkedin Login

--- KonyAccelerate/controllers/mobile/frmLoginController/frmLoginControllerActions.js
+++ KonyAccelerate/controllers/mobile/frmLoginController/frmLoginControllerActions.js
@@ -3,13 +3,13 @@ define({
       This is an auto generated file and any modifications to it may result in corruption of the action sequence.
     */
     /** onLoginSuccess defined for linkedinlogin **/
-    AS_UWI_cd1d5bbf76d24a82a366b67f8ef22bb6: function AS_UWI_cd1d5bbf76d24a82a366b67f8ef22bb6(response) {
+    AS_UWI_f5b772abfeca4d809b68f66f91b61a75: function AS_UWI_f5b772abfeca4d809b68f66f91b61a75(response) {
         var self = this;
         var ntf = new kony.mvc.Navigation("frmEventList");
         ntf.navigate();
     },
     /** onLoginFailure defined for linkedinlogin **/
-    AS_UWI_j7f038a6352244d39e78302c0ecffcd0: function AS_UWI_j7f038a6352244d39e78302c0ecffcd0(response) {
+    AS_UWI_ge777c6098f545e693c0537e7f6d99e6: function AS_UWI_ge777c6098f545e693c0537e7f6d99e6(response) {
         var self = this;
         alert("login failed");
     }
