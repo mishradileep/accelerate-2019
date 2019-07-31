4d474012906cb0ec43a35985384d502347175375
Krishna Kanth Majety <krishnakanth.majety@kony.com>
2019-07-31 17:57:42 +0530

    [MARVP-30] Implement Wifi

--- KonyAccelerate/controllers/mobile/frmWifiInfoController/frmWifiInfoController.js
+++ KonyAccelerate/controllers/mobile/frmWifiInfoController/frmWifiInfoController.js
@@ -10,47 +10,14 @@ define({
         * @private
     */
     formPreShowAction: function() {
-        var objectServiceName = "KonyAccelerateStorageService";
-        var dataObject = "wifi_info";
-        var queryParams = {
-            "$filter": "((SoftDeleteFlag ne true) or (SoftDeleteFlag eq null))"
-        };
-        if (!isWifiInfoFetched) {
-            kony.application.showLoadingScreen(null, "Fetching Data");
-            fetchObjectData(objectServiceName, dataObject, queryParams, this.wifiInfoFetchSuccess.bind(this),
-                this.wifiInfoFetchFailure.bind(this));
-        } else {
-            this.view.lblNetworkName.text = wifiName;
-            this.view.lblPasswordValue.text = wifiPassword;
-            this.view.forceLayout();
-        }
-    },
-
-    /**
-     * @function wifiInfoFetchSuccess
-     * @description This is the success callback of the wifi information fetch call
-     * @param successResponse The success response of the wifiFetch object
-     * @private
-     */
-    wifiInfoFetchSuccess: function(successResponse) {
-        kony.application.dismissLoadingScreen();
-        let wifiInfo = successResponse.records[0];
-        let wifiName = wifiInfo.wifi_name;
-        let wifiPassword = wifiInfo.wifi_password;
-        this.view.lblNetworkName.text = wifiName;
+      let accelerateEventData = kony.store.getItem("accelerateEventData");
+      if(!kony.sdk.isNullOrUndefined(accelerateEventData)){
+        let wifiInformation = accelerateEventData.wifiInformation;
+        let wifiUserName = (wifiInformation.hasOwnProperty("wifi_name")) ? wifiInformation.wifi_name : "";
+        let wifiPassword = (wifiInformation.hasOwnProperty("wifi_password")) ? wifiInformation.wifi_password : "";
+        this.view.lblNetworkName.text = wifiUserName;
         this.view.lblPasswordValue.text = wifiPassword;
-    },
-
-    /**
-     * @function wifiInfoFetchFailure
-     * @description This is the error callback of the wifi information fetch call
-     * @param failureResponse The failure response of the wifiFetch object
-     * @private
-     */
-    wifiInfoFetchFailure: function(failureResponse) {
-        kony.application.dismissLoadingScreen();
-        kony.print("Exception occured while fetching the wifi information");
-        kony.print("Exception occured is " + JSON.stringify(failureResponse));
+      }
     },
 
 });
