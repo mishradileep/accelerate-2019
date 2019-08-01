d9a2564df0b60e8a3760506f25d45e45035e64f0
Dileep Mishra <mishradileep@gmail.com>
2019-08-01 19:27:53 +0530

    [MARVP-000] Fix for Wifi Animation, Unhandled exceptations and missing actions

--- KonyAccelerate/actions/mobile/appEvents.js
+++
@@ -1,6 +0,0 @@
-define({
-    AS_AppEvents_c081992218f44bf89cef587f76549c96: function AS_AppEvents_c081992218f44bf89cef587f76549c96(eventobject) {
-        var self = this;
-        appPreShow();
-    }
-});
