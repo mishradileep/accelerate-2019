a6b2afcdc2fcc629a6a3969d412d4589de3f00ac
Krishna Kanth Majety <krishnakanth.majety@kony.com>
2019-08-01 18:43:14 +0530

    [MARVP-37] Address PR Comments

--- KonyAccelerate/controllers/mobile/frmEventLocationController/frmEventLocationController.js
+++ KonyAccelerate/controllers/mobile/frmEventLocationController/frmEventLocationController.js
@@ -1,5 +1,5 @@
 define({
-  
+
     /**
      * @function formEventLocationPostShowAction
      * @descripiton This function is invoked in the pre show of the form which is used to fetch the event data
@@ -7,78 +7,81 @@ define({
      */
     formEventLocationPostShowAction: function() {
         let eventData = kony.store.getItem("accelerateEventData");
-      	if(!kony.sdk.isNullOrUndefined(eventData)){
-          let eventLocationData = eventData.hasOwnProperty("eventLocationData") ? eventData.eventLocationData : null;
-          if (eventLocationData !== null) {
-             this.formatAndSetEventData(eventLocationData);
-          } else {
-             kony.print("Event Location Data is null");
-          }
-        }else{
-           var queryParams = {
-            "$filter": "((SoftDeleteFlag ne true) or (SoftDeleteFlag eq null))",
-            "$expand": "event_inner_location,wifi_info,location"
-          };
-          fetchObjectData(eventConstants.OBJECT_SERVICE_NAME, eventConstants.EVENT_OBJECT_NAME, queryParams, this.eventFetchSuccess.bind(this), this.eventFetchFailure.bind(this));
+        if (!kony.sdk.isNullOrUndefined(eventData)) {
+            let eventLocationData = eventData.hasOwnProperty("eventLocationData") ? eventData.eventLocationData : null;
+            if (eventLocationData !== null) {
+                this.formatAndSetEventData(eventLocationData);
+            } else {
+                kony.print("Event Location Data is null");
+            }
+        } else {
+            var queryParams = {
+                "$filter": "((SoftDeleteFlag ne true) or (SoftDeleteFlag eq null))",
+                "$expand": "event_inner_location,wifi_info,location"
+            };
+            fetchObjectData(eventConstants.OBJECT_SERVICE_NAME, eventConstants.EVENT_OBJECT_NAME,
+                queryParams,
+                this.eventFetchSuccess.bind(this),
+                this.eventFetchFailure.bind(this));
         }
     },
-  	
-  	 /**
+
+    /**
      * @function eventFetchSuccess
      * @descripiton This function is invoked in the success response of event object
      * @param successResponse The success response of the event object
      * @private
      */
-  	eventFetchSuccess : function(successResponse){
-      let eventLocationData = successResponse.hasOwnProperty("records") ? successResponse.records : null;
-      if(eventLocationData !== null){
-        let eventLocation = eventLocationData[0].location[0];
-        this.formatAndSetEventData(eventLocation);
-      }else{
-        kony.print("Fetched event data is null");
-      }
+    eventFetchSuccess: function(successResponse) {
+        let eventLocationData = successResponse.hasOwnProperty("records") ? successResponse.records : null;
+        if (eventLocationData !== null) {
+            let eventLocation = eventLocationData[0].location[0];
+            this.formatAndSetEventData(eventLocation);
+        } else {
+            kony.print("Fetched event data is null");
+        }
     },
-  
-  	 /**
+
+    /**
      * @function eventFetchFailure
      * @descripiton This function is invoked in the failure response of event object
      * @param failureResponse The failure response of the event object
      * @private
      */
-  	eventFetchFailure : function(failureResponse){
-      kony.print(eventConstants.GENERIC_EXCEPTION_MESSAGE);
-      kony.print("Failure response is "+JSON.stringify(failureResponse));
-    },	
-  
-  	 /**
+    eventFetchFailure: function(failureResponse) {
+        kony.print(eventConstants.GENERIC_EXCEPTION_MESSAGE);
+        kony.print("Failure response is " + JSON.stringify(failureResponse));
+    },
+
+    /**
      * @function formatAndSetEventData
      * @descripiton This function is used to format the service data and set the data
      * @param eventLocationData The event location data
      * @private
      */
-  	formatAndSetEventData : function(eventLocationData){
-       let hotelName = eventLocationData.hasOwnProperty("location") ? eventLocationData.location : "";
-       let completeAddress = this.getCompleteAddress(eventLocationData);
-       let phoneNumber = eventLocationData.hasOwnProperty("phone_number") ? eventLocationData.phone_number : "";
-       let coOrdinatesInfo = this.getCoordinates(eventLocationData);
-       this.setEventData(hotelName, completeAddress, phoneNumber, coOrdinatesInfo);
+    formatAndSetEventData: function(eventLocationData) {
+        let hotelName = eventLocationData.hasOwnProperty("location") ? eventLocationData.location : "";
+        let completeAddress = this.getCompleteAddress(eventLocationData);
+        let phoneNumber = eventLocationData.hasOwnProperty("phone_number") ? eventLocationData.phone_number : "";
+        let coordinatesInfo = this.getCoordinates(eventLocationData);
+        this.setEventData(hotelName, completeAddress, phoneNumber, coordinatesInfo);
     },
-  	
-  	 /**
+
+    /**
      * @function getCompleteAddress
      * @descripiton This function is used to format and construct the complete address
      * @param eventLocationData The event location data
      * @private
      */
-  	getCompleteAddress : function(eventLocationData){
-       let addressLine1 = eventLocationData.hasOwnProperty("addressLine1") ? eventLocationData.addressLine1 : "";
-       let cityName = eventLocationData.hasOwnProperty("cityname") ? eventLocationData.cityname : "";
-       let pincode = eventLocationData.hasOwnProperty("pincode") ? eventLocationData.pincode : "";
-       let countryName = eventLocationData.hasOwnProperty("country_name") ? eventLocationData.country_name : "";
-       let completeAddress = addressLine1 + ", " + cityName + ", " + pincode + ", " + countryName;
-       return completeAddress;
+    getCompleteAddress: function(eventLocationData) {
+        let addressLine1 = eventLocationData.hasOwnProperty("addressLine1") ? eventLocationData.addressLine1 : "";
+        let cityName = eventLocationData.hasOwnProperty("cityname") ? eventLocationData.cityname : "";
+        let pincode = eventLocationData.hasOwnProperty("pincode") ? eventLocationData.pincode : "";
+        let countryName = eventLocationData.hasOwnProperty("country_name") ? eventLocationData.country_name : "";
+        let completeAddress = addressLine1 + ", " + cityName + ", " + pincode + ", " + countryName;
+        return completeAddress;
     },
-  
+
     /**
      * @function getCoordinates
      * @descripiton This function is used to construct the coOrdinates Information
@@ -86,12 +89,12 @@ define({
      * @private
      */
     getCoordinates: function(eventLocationData) {
-        let coOrdinatesInfo = {};
+        let coordinatesInfo = {};
         let latitude = eventLocationData.hasOwnProperty("latitude") ? eventLocationData.latitude : "";
         let longitude = eventLocationData.hasOwnProperty("longitude") ? eventLocationData.longitude : "";
-        coOrdinatesInfo.latitude = latitude;
-        coOrdinatesInfo.longitude = longitude;
-        return coOrdinatesInfo;
+        coordinatesInfo.latitude = latitude;
+        coordinatesInfo.longitude = longitude;
+        return coordinatesInfo;
     },
     /**
      * @function setEventData
@@ -99,32 +102,32 @@ define({
      * @param hotelName The name of the hotel
      * @param completeAddress The complete address of the event location
      * @param phoneNumber The phone number of the event location
-     * @param coOrdinatesInfo The information about the co-ordinates of the event location
+     * @param coordinatesInfo The information about the co-ordinates of the event location
      * @private
      */
-    setEventData: function(hotelName, completeAddress, phoneNumber, coOrdinatesInfo) {
+    setEventData: function(hotelName, completeAddress, phoneNumber, coordinatesInfo) {
         kony.print("Mapping the event location data to UI elements");
         this.view.lblHotelName.text = hotelName;
         this.view.lblHotelInfo.text = completeAddress;
         this.view.lblHotelPhone.text = phoneNumber;
-        this.setDataToMap(hotelName, completeAddress, coOrdinatesInfo);
-      	this.animateLocationCard("23%",false);
+        this.setDataToMap(hotelName, completeAddress, coordinatesInfo);
+        this.animateLocationCard("23%", false);
         kony.print("Mapping the event location data to UI elements completed");
     },
-  
+
     /**
      * @function setDataToMap
      * @descripiton This function is used to set the event location in the map widget
      * @param hotelName The name of the hotel
      * @param completeAddress The complete address of the event location
-     * @param coOrdinatesInfo The information about the co-ordinates of the event location
+     * @param coordinatesInfo The information about the co-ordinates of the event location
      * @private
      */
-    setDataToMap: function(hotelName, completeAddress, coOrdinatesInfo) {
+    setDataToMap: function(hotelName, completeAddress, coordinatesInfo) {
         var locationPin = {
             id: "accelerateEvent", // id is mandatory for every pin
-            lat: coOrdinatesInfo.latitude,
-            lon: coOrdinatesInfo.longitude,
+            lat: coordinatesInfo.latitude,
+            lon: coordinatesInfo.longitude,
             name: hotelName,
             image: "defaultImage.png",
             focusImage: "focusImage.png",
@@ -139,18 +142,23 @@ define({
         this.view.mapEventLocation.addPin(locationPin);
         this.view.mapEventLocation.navigateTo(0, false);
     },
-  	
-  	onCloseButtonClick : function(){
-      this.animateLocationCard("100%", true)
+
+    /**
+     * @function onCLoseButtonClick
+     * @descripiton This function is used to close the location page and navigate back to more page
+     * @private
+     */
+    onCloseButtonClick: function() {
+        this.animateLocationCard("100%", true);
     },
-  
-  	 /**
+
+    /**
      * @function animateLocationCard
      * @descripiton This function is used to animate the card from bottom to top
      * @param finalTop The top position of the flex
      * @private
      */
-  	animateLocationCard: function(finalTop,isClose) {
+    animateLocationCard: function(finalTop, isClose) {
         this.view.flxMapContainer.animate(
             kony.ui.createAnimation({
                 100: {
@@ -163,8 +171,8 @@ define({
                 duration: 0.5
             }, {
                 animationEnd: function() {
-					if(isClose){
-                      new kony.mvc.Navigation("frmMore").navigate();
+                    if (isClose) {
+                        new kony.mvc.Navigation("frmMore").navigate();
                     }
                 }
             });
