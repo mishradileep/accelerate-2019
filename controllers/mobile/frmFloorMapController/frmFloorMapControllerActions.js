321357f7d951bae678ed23ed314682c0d08a446d
Krishna Kanth Majety <krishnakanth.majety@kony.com>
2019-08-06 11:37:49 +0530

    [MARVP-32] Initial Commit of Floor Map

---
+++ controllers/mobile/frmFloorMapController/frmFloorMapControllerActions.js
@@ -0,0 +1,10 @@
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    /** onClick defined for flxFloorMapList **/
+    AS_FlexContainer_fdff18af953b403f97231e492edd8ada: function AS_FlexContainer_fdff18af953b403f97231e492edd8ada(eventobject) {
+        var self = this;
+        this.view.sgmntFloorMap.isVisible = true;
+    }
+});
