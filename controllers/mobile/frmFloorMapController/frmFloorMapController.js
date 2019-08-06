d32be9b70438e64e6cf58a0b41db0ec9ba864989
Krishna Kanth Majety <krishnakanth.majety@kony.com>
2019-08-06 13:07:43 +0530

    [MARVP-32] Initial Commit of Floor Map

--- controllers/mobile/frmFloorMapController/frmFloorMapController.js
+++ controllers/mobile/frmFloorMapController/frmFloorMapController.js
@@ -1,5 +1,21 @@
 define({ 
 
- //Type your controller code here 
+  segmentData : null,
+  formFloorMapPreshowAction : function(){
+    let eventData = accelerateEventData.eventdata.records[0];
+    if (!kony.sdk.isNullOrUndefined(eventData)) {
+      let eventInnerLocation = eventData.event_inner_location;
+      alert(eventInnerLocation);
+      if(eventInnerLocation !== null){
+           var segmentWidgetDataMap = {
+             "lblListItem":"floor_name"
+           };
+       this.view.sgmntFloorMap.widgetDataMap = segmentWidgetDataMap;
+       this.segmentData = eventInnerLocation;
+       this.view.sgmntFloorMap.setData(eventInnerLocation);
+       this.view.sgmntFloorMap.isVisible=true;
+      }
+    }
+  }
 
  });
