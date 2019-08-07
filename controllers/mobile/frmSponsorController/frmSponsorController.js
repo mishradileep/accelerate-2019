fd237f148a8642150fcda1f9f07a6ea14b6b25c1
Krishna Kanth Majety <krishnakanth.majety@kony.com>
2019-08-07 13:41:09 +0530

    [MARVP-49] Beautified Code and add function comments

--- controllers/mobile/frmSponsorController/frmSponsorController.js
+++ controllers/mobile/frmSponsorController/frmSponsorController.js
@@ -1,82 +1,122 @@
 define({
 
+    /**
+     * @function formSponsorPreshow
+     * @description Invoked in the preshow of the form
+     * @private
+     */
     formSponsorPreshow: function() {
         let sponsorData = accelerateSponsorData.sponsorData.records;
-      	this.setSponsorData(sponsorData);
-    },
-
-    setSponsorData: function(sponsorsData) {
-        let formattedData = this.formatCollectionViewData(sponsorsData);
+        let formattedData = this.formatCollectionViewData(sponsorData);
         this.createCollectionView(formattedData);
     },
 
-  	animateSponsorsFlex : function(finalTop){
-      this.view.flxMainContainer.animate(
-      kony.ui.createAnimation({100: {top: finalTop, "stepConfig": {}}}),
-      {delay: 0, fillMode: kony.anim.FILL_MODE_FORWARDS, duration: 0.5},
-      {animationEnd: function() {
-        if(finalTop === "100%"){
-          new kony.mvc.Navigation("frmMore").navigate();
-        }
-      }});
+    /**
+     * @function animateSponsorsFlex
+     * @description Used to animate the sponsors flex based on the top value
+     * @param finalTop The top value to which the flex needs to be animated
+     * @private
+     */
+    animateSponsorsFlex: function(finalTop) {
+        this.view.flxMainContainer.animate(
+            kony.ui.createAnimation({
+                100: {
+                    top: finalTop,
+                    "stepConfig": {}
+                }
+            }), {
+                delay: 0,
+                fillMode: kony.anim.FILL_MODE_FORWARDS,
+                duration: 0.5
+            }, {
+                animationEnd: function() {
+                    if (finalTop === "100%") {
+                        new kony.mvc.Navigation("frmMore").navigate();
+                    }
+                }
+            });
     },
-  
-  	formatCollectionViewData : function(sponsorsData) {
-  		let collectionViewData =[];
-      	for(let index = 0 ; index < sponsorsData.length ; index++){
-          let currentSponsorObject = [];
-          let currentSponsorData = sponsorsData[index];
-          let sponsorData = currentSponsorData.sponsors;
-          let updatedSponsorData;
-          updatedSponsorData = this.formatData(sponsorData,sponsorData.length);
-          currentSponsorObject = [{
-            "categoryName":{
-             "text":currentSponsorData.categoryName
-            }
-          },sponsorData,{}];
-          collectionViewData.push(currentSponsorObject);
+
+    /**
+     * @function formatCollectionViewData
+     * @description Used to format the data according to the collection view widget data format
+     * @param sponsorsData The data of sponsors
+     * @private
+     */
+    formatCollectionViewData: function(sponsorsData) {
+        let collectionViewData = [];
+        for (let index = 0; index < sponsorsData.length; index++) {
+            let currentSponsorObject = [];
+            let currentSponsorData = sponsorsData[index];
+            let sponsorData = currentSponsorData.sponsors;
+            let updatedSponsorData;
+            updatedSponsorData = this.formatData(sponsorData, sponsorData.length);
+            currentSponsorObject = [{
+                "categoryName": {
+                    "text": currentSponsorData.categoryName
+                }
+            }, sponsorData, {}];
+            collectionViewData.push(currentSponsorObject);
         }
-      	return collectionViewData;
+        return collectionViewData;
     },
-  
+
+    /**	
+     * @function createCollectionView
+     * @description Used to create the collection view widget and add it to the form
+     * @param categoryData The sponsor data
+     * @private
+     */
     createCollectionView: function(categoryData) {
         var bsp = this.getBSPConfig();
         var psp = this.getPSPConfig();
         var lsp = this.getLSPConfig();
-      	bsp.data = categoryData; 
-        var collectionView = new kony.ui.CollectionView(bsp,lsp,psp);
-      	this.view.flxMainContainer.add(collectionView);
-      	this.view["sponsorCollectionView"].setVisibility(true);
-      	this.view.forceLayout();
+        bsp.data = categoryData;
+        var collectionView = new kony.ui.CollectionView(bsp, lsp, psp);
+        this.view.flxMainContainer.add(collectionView);
+        this.view["sponsorCollectionView"].setVisibility(true);
+        this.view.forceLayout();
     },
-	
-  	formatData : function(categoryData,length){
-      let width = (length > 1) ? "49%" : "96%";
-      for(let index = 0 ; index < categoryData.length ; index++){
-        categoryData[index].flxSponsor = {
-          "width" : width,
-          "top":"5dp",
-        };
-        var dummyTemplate ={
-          "itemTemplate":"flxDummySponsor",
-           "flxSponsor" :{
-          		"width":"49%"
-        	}
-        };
-        if(length % 2 === 1 && length > 1){
-          categoryData[length] = dummyTemplate
+
+    /**
+     * @function formatData
+     * @description Used to format the data according to the collection view section data format
+     * @param categoryData The sponsor data of the particular category
+     * @param length The number of sponsors in the specific category
+     * @private
+     */
+    formatData: function(categoryData, length) {
+        let width = (length > 1) ? "49%" : "96%";
+        for (let index = 0; index < categoryData.length; index++) {
+            categoryData[index].flxSponsor = {
+                "width": width,
+                "top": "5dp",
+            };
+            var dummyTemplate = {
+                "itemTemplate": "flxDummySponsor",
+                "flxSponsor": {
+                    "width": "49%"
+                }
+            };
+            if (length % 2 === 1 && length > 1) {
+                categoryData[length] = dummyTemplate
+            }
         }
-      }
-      return categoryData;
+        return categoryData;
     },
-  
+
+    /**
+     * @function getBSPConfig
+     * @description Used to fetch the basic properties of collection view
+     * @private
+     */
     getBSPConfig: function() {
         return {
             "autogrowMode": kony.flex.AUTOGROW_NONE,
             "data": null,
             "left": "5%",
             "top": "5dp",
-          	"bottom":"5dp",
+            "bottom": "5dp",
             "width": "90%",
             "height": "98%",
             "id": "sponsorCollectionView",
@@ -86,20 +126,25 @@ define({
             "retainSelection": true,
             "skin": "sknFullFlex",
             "itemTemplate": "flxSponsor",
-          	"sectionHeaderTemplate":"flxSponsorCategory",
+            "sectionHeaderTemplate": "flxSponsorCategory",
             "scrollingEvents": {},
             "sectionFooterTemplate": null,
             "showScrollbars": false,
             "layout": kony.collectionview.LAYOUT_HORIZONTAL,
             "widgetDataMap": {
                 "imgSponsorIcon": "sponsorImageURL",
-              	"flxSponsor":"flxSponsor",
-              	"lblSponsorCategoryName":"categoryName",
+                "flxSponsor": "flxSponsor",
+                "lblSponsorCategoryName": "categoryName",
             },
             "zIndex": 1,
         };
     },
 
+    /**
+     * @function getPSPConfig
+     * @description Used to fetch the platform specific properties of collection view
+     * @private
+     */
     getPSPConfig: function() {
         return {
             "bounces": true,
@@ -107,6 +152,11 @@ define({
         };
     },
 
+    /**
+     * @function getLSPConfig
+     * @description Used to fetch the layout specific properties of collection view
+     * @private
+     */
     getLSPConfig: function() {
         return {
             "padding": [0, 0, 0, 0],
