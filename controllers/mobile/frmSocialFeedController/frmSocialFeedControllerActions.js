cafdb735c842e33c05a50745e188670b043077cc
KonyDev <vigneshwaran.karunanithi@kony.com>
2019-08-12 16:34:56 +0530

    [MARC-54] implement social feed. Added new form form social feed Add navigation to social feed from more option Add functionlities in social feed controller

---
+++ controllers/mobile/frmSocialFeedController/frmSocialFeedControllerActions.js
@@ -0,0 +1,15 @@
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    /** onCloseClick defined for mobileheader **/
+    AS_UWI_baceaf2289ba434c86d9fda05f53522c: function AS_UWI_baceaf2289ba434c86d9fda05f53522c(eventobject) {
+        var self = this;
+        this.animatePDFViewer(100);
+    },
+    /** preShow defined for frmSocialFeed **/
+    AS_Form_a7ef91fff87a4bada03398b8d9779865: function AS_Form_a7ef91fff87a4bada03398b8d9779865(eventobject) {
+        var self = this;
+        this.frmSocialFeedPreshowAction();
+    }
+});
