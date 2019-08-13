ea0a2bc75e87c2f58d899dc81ea1a10a66e77f06
Dileep Mishra <mishradileep@gmail.com>
2019-08-13 18:37:43 +0530

    [MARVP-127] Fix Welcome screen AJAX loader

--- controllers/mobile/frmWelcomeController/frmWelcomeControllerActions.js
+++ controllers/mobile/frmWelcomeController/frmWelcomeControllerActions.js
@@ -5,6 +5,7 @@ define({
     /** onTouchStart defined for flxContinue **/
     AS_FlexContainer_d356f394296842338f99e3d7e22f5804: function AS_FlexContainer_d356f394296842338f99e3d7e22f5804(eventobject, x, y) {
         var self = this;
+        this.animateWelcomeArrow();
         syncEventData();
     }
 });
