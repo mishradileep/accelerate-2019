55f39ab6a4b4fffc47b00951e187de704339edad
Vijay Samanthapudi <vijay.samanthapudi@kony.com>
2019-08-14 17:17:31 +0530

    [MARVP-154] events not filtering

--- controllers/mobile/frmAgendaController/frmAgendaController.js
+++ controllers/mobile/frmAgendaController/frmAgendaController.js
@@ -852,6 +852,7 @@ define({
      */
     agendaFilter: function(eventobject) {
       	this.view.contentScroller.setContentOffset({"y":0}, true);
+      	this.changeButtonSkins("4TH SEP");
         var self = this;
         var leftPos = "0%";
         var buttonText = "ALL";
@@ -1577,7 +1578,12 @@ define({
       }
     }
     if(found){
-      this.view.contentScroller.scrollToWidget(sessions[index],true);
+      if(index===0){
+        this.view.contentScroller.setContentOffset({"y":0},true);
+      }
+      else{
+        this.view.contentScroller.scrollToWidget(sessions[index],true);
+      }
     }
     else{
       this.view.contentScroller.scrollToEnd();
