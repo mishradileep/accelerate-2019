3e6a6f364f45774a9bf2950169dfe9f30755d18a
Krishna Kanth <krishnakanth.majety@kony.com>
2019-08-16 19:50:27 +0530

    [MARVP-215] Implement About the App page

---
+++ controllers/mobile/frmPrivacyPolicyController/frmPrivacyPolicyControllerActions.js
@@ -0,0 +1,11 @@
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    /** onCloseClick defined for mobileheader **/
+    AS_UWI_cbfff32362f8462ea4ac0c2ee8b88561: function AS_UWI_cbfff32362f8462ea4ac0c2ee8b88561(eventobject) {
+        var self = this;
+        var ntf = new kony.mvc.Navigation("frmAboutApp");
+        ntf.navigate();
+    }
+});
